56ab0d6ea7e999f2f5e5b0de3ebb78a6e2e93088
akMane96 <120082033+akMane96@users.noreply.github.com>
2022-12-08 12:09:49 +0530

    Add files via upload

---
+++ 01-Windows/Fixed Functional/26- LookAt/LookAt.cpp
@@ -0,0 +1,390 @@
+//header files
+#include <windows.h>
+#include <gl/gl.h>
+#include <gl/glu.h>
+#include<stdio.h>
+
+#include "icon.h"
+
+//Macros
+#define  ASM_WIN_WIDTH 800
+#define ASM_WIN_HEIGHT 600
+
+//PragmaASM_WIN_WIDTH
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib,"glu32.lib")
+
+//Global Function Declaration
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//Global Variable Declaration
+DWORD ASM_dwStyle;
+WINDOWPLACEMENT ASM_wpPrev = { sizeof(WINDOWPLACEMENT) };
+HWND ASM_ghwnd = NULL;
+HDC ASM_ghdc = NULL;
+HGLRC ASM_ghrc;
+bool ASM_gbFullScreen = false;
+bool ASM_gbActiveWindow = false;
+FILE *ASM_gpFile = NULL;
+
+//Win MAin
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	//Local Function Declaration
+	void Display();
+	void Initialize();
+
+	//Local Variable Declaration
+	WNDCLASSEX ASM_wndclass;
+	HWND ASM_hwnd;
+	MSG ASM_msg;
+	TCHAR ASM_szAppName[] = TEXT("ASM Window");
+	bool ASM_iDone = false;
+	
+	//code
+
+	if (fopen_s(&ASM_gpFile, "ASMLog.txt", "w") != 0)
+	{
+		MessageBox(NULL, TEXT("Cant Create Log File"), TEXT("ERROR"), MB_OK | MB_ICONWARNING);
+		exit(0);
+	}
+	else
+	{
+		fprintf(ASM_gpFile, "Log Opened Programm started Succssfully !!!!!\n");
+	}
+
+	//Fill WNDCLASSEX struct
+	ASM_wndclass.cbSize = sizeof(WNDCLASSEX);
+	ASM_wndclass.cbClsExtra = 0;
+	ASM_wndclass.cbWndExtra = 0;
+	ASM_wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+
+	ASM_wndclass.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(ASMICON));
+	ASM_wndclass.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(ASMICON));
+	ASM_wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	ASM_wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+
+
+	ASM_wndclass.lpszClassName = ASM_szAppName;
+	ASM_wndclass.lpszMenuName = NULL;
+	ASM_wndclass.lpfnWndProc = WndProc;
+
+
+	//Register Class to OS
+	RegisterClassEx(&ASM_wndclass);
+
+	
+	//Create Window
+	ASM_hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+		ASM_szAppName,
+		TEXT("Akshay LookAt OpenGL"),
+		WS_OVERLAPPEDWINDOW,
+		(GetSystemMetrics(SM_CXSCREEN) / 2) - (ASM_WIN_WIDTH / 2),
+		(GetSystemMetrics(SM_CYSCREEN) / 2) - (ASM_WIN_HEIGHT / 2),
+		ASM_WIN_WIDTH,
+		ASM_WIN_HEIGHT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ASM_ghwnd = ASM_hwnd;
+
+	//Intialize
+	Initialize();
+
+	//Show Window
+	ShowWindow(ASM_hwnd, iCmdShow);
+
+	//Set Focus
+	SetForegroundWindow(ASM_hwnd);
+	SetFocus(ASM_ghwnd);	
+
+	//Game Loop
+
+	while (ASM_iDone == false)
+	{
+		if (PeekMessage(&ASM_msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (ASM_msg.message == WM_QUIT)
+			{
+				ASM_iDone = true;
+			}
+			else
+			{
+				TranslateMessage(&ASM_msg);
+				DispatchMessage(&ASM_msg);
+			}
+		}
+		else
+		{
+			if (ASM_gbActiveWindow == true)
+			{
+				Display();
+			}
+		}
+	}
+
+	return((int)ASM_msg.wParam);
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//Local Fuction Declaration
+	void ToggleFullScreen();
+	void Resize(int, int);
+	void UnInitialize();
+	
+	switch (iMsg)
+	{
+		case WM_SETFOCUS:
+			ASM_gbActiveWindow = true;
+			break;
+
+		case WM_KILLFOCUS:
+			ASM_gbActiveWindow = false;
+			break;
+
+		case WM_ERASEBKGND:
+			return(0);
+
+		case WM_SIZE:
+			Resize(LOWORD(lParam), HIWORD(lParam));
+			break;
+
+		case WM_KEYDOWN:
+
+			switch(wParam)
+			{
+				case VK_ESCAPE:
+					DestroyWindow(ASM_ghwnd);
+					break;
+
+				case 0x46:
+				case 0x66:
+					ToggleFullScreen();
+					break;
+
+				default:
+					break;
+			}
+
+			break;
+
+		case WM_CLOSE:
+			DestroyWindow(ASM_ghwnd);
+			break;
+
+		case WM_DESTROY:
+			UnInitialize();
+			PostQuitMessage(0);
+			break;
+	}
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+void ToggleFullScreen()
+{
+	MONITORINFO ASM_mi = {sizeof(MONITORINFO)};
+
+	if (ASM_gbFullScreen == false)
+	{
+
+		ASM_dwStyle = GetWindowLong(ASM_ghwnd, GWL_STYLE);
+
+		if (ASM_dwStyle & WS_OVERLAPPEDWINDOW)
+		{
+			if (GetWindowPlacement(ASM_ghwnd, &ASM_wpPrev) &&
+				GetMonitorInfo(MonitorFromWindow(ASM_ghwnd, MONITORINFOF_PRIMARY), &ASM_mi))
+			{
+				SetWindowLong(ASM_ghwnd, GWL_STYLE, (ASM_dwStyle & ~WS_OVERLAPPEDWINDOW));
+
+				SetWindowPos(ASM_ghwnd,
+					HWND_TOP,
+					ASM_mi.rcMonitor.left,
+					ASM_mi.rcMonitor.top,
+					(ASM_mi.rcMonitor.right - ASM_mi.rcMonitor.left),
+					(ASM_mi.rcMonitor.bottom - ASM_mi.rcMonitor.top),
+					SWP_NOZORDER | SWP_FRAMECHANGED
+				);
+
+				ShowCursor(FALSE);
+				ASM_gbFullScreen = true;
+			}
+
+
+		}
+	}
+		else
+		{
+			SetWindowLong(ASM_ghwnd, GWL_STYLE, ASM_dwStyle | WS_OVERLAPPEDWINDOW);
+
+			SetWindowPlacement(ASM_ghwnd, &ASM_wpPrev);
+
+			SetWindowPos(ASM_ghwnd,
+				HWND_TOP,
+				0,
+				0,
+				0,
+				0, SWP_NOZORDER | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER
+				);
+
+			ShowCursor(TRUE);
+			ASM_gbFullScreen = false;
+		}
+}
+
+void Initialize()
+{
+	//Local Function Declaration
+	void Resize(int, int);
+
+	//Local variable Declaration
+	PIXELFORMATDESCRIPTOR ASM_pfd;
+	int ASM_iPixelFormatIndex ;
+
+	//code
+
+	ASM_ghdc = GetDC(ASM_ghwnd);
+
+	ZeroMemory(&ASM_pfd, sizeof(PIXELFORMATDESCRIPTOR));
+
+	ASM_pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	ASM_pfd.nVersion = 1;
+	ASM_pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	ASM_pfd.iPixelType = PFD_TYPE_RGBA;
+	ASM_pfd.cColorBits = 32;
+	ASM_pfd.cRedBits = 8;
+	ASM_pfd.cGreenBits = 8;
+	ASM_pfd.cBlueBits = 8;
+	ASM_pfd.cAlphaBits = 8;
+
+	ASM_iPixelFormatIndex = ChoosePixelFormat(ASM_ghdc, &ASM_pfd);
+
+	if (ASM_iPixelFormatIndex == 0)
+	{
+		fprintf(ASM_gpFile, "ChoosePixelFormat() Failed !!!!!\n");
+		DestroyWindow(ASM_ghwnd);
+	}
+
+	if (SetPixelFormat(ASM_ghdc, ASM_iPixelFormatIndex, &ASM_pfd) == FALSE)
+	{
+		fprintf(ASM_gpFile, "SetPixelFormat() Failed !!!!!\n");
+		DestroyWindow(ASM_ghwnd);
+	}
+
+	ASM_ghrc = wglCreateContext(ASM_ghdc);
+
+	if (ASM_ghrc == NULL)
+	{
+		fprintf(ASM_gpFile, "wglCreateContext() Failed !!!!!\n");
+		DestroyWindow(ASM_ghwnd);
+	}
+
+	if (wglMakeCurrent(ASM_ghdc, ASM_ghrc) == FALSE)
+	{
+		fprintf(ASM_gpFile, "wglMakeCurrent() Failed !!!!!\n");
+		DestroyWindow(ASM_ghwnd);
+	}
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	//Warm Up Resize
+
+	Resize(ASM_WIN_WIDTH, ASM_WIN_HEIGHT);
+	
+}
+
+
+void Resize(int width, int height)
+{
+	//code
+	if (height <= 0)
+		height = 1;
+
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+
+}
+
+void Display()
+{
+	//code
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	gluLookAt(0.0f, 0.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(0.0f, 1.0f, 0.0f);
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-1.0f, -1.0f, 0.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 0.0f);
+
+	glEnd();
+	
+	SwapBuffers(ASM_ghdc);
+
+}
+
+void UnInitialize()
+{
+
+	if(ASM_gbFullScreen == true)
+	{
+		SetWindowLong(ASM_ghwnd, GWL_STYLE, ASM_dwStyle | WS_OVERLAPPEDWINDOW);
+
+		SetWindowPlacement(ASM_ghwnd, &ASM_wpPrev);
+
+		SetWindowPos(ASM_ghwnd,
+			HWND_TOP,
+			0,
+			0,
+			0,
+			0, SWP_NOZORDER | SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER
+		);
+
+		ShowCursor(TRUE);
+		ASM_gbFullScreen = false;
+
+	}
+
+	if (wglGetCurrentContext() == ASM_ghrc)
+	{
+		wglMakeCurrent(NULL, NULL);
+	}
+
+	if (ASM_ghrc)
+	{
+		wglDeleteContext(ASM_ghrc);
+		ASM_ghrc = NULL;
+	}
+
+	if (ASM_ghdc)
+	{
+		ReleaseDC(ASM_ghwnd, ASM_ghdc);
+		ASM_ghdc = NULL;
+	}
+
+	if (ASM_gpFile)
+	{
+		fprintf(ASM_gpFile, "Log Closed Program closed Succssfully !!!!!\n");
+		fclose(ASM_gpFile);
+		ASM_gpFile = NULL;
+	}
+}
